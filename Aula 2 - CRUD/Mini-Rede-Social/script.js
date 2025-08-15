68bfee700bcb2ffa4423e2fb10d755cf895825bb
Mateus Macedo <mateusmacedobatista05@gmail.com>
2025-08-14 22:32:30 -0300

    commit

--- Aula 2 - CRUD/Mini-Rede-Social/script.js
+++ Aula 2 - CRUD/Mini-Rede-Social/script.js
@@ -18,3 +18,54 @@ let posts = [
         date: "12/10/2023 12:00:00"
     }
 ];
+
+window.onload = function() {
+    mostrarPost();
+    document.querySelector("#postForm").addEventListener("submit", addPost)
+}
+
+//create
+    function addPost(infosDoEvento) {
+        infosDoEvento.preventDefault();
+
+        const textPost = document.querySelector("#postText").value;
+        const postCategory = document.querySelector("#postCategory").value;
+        const postImage = document.querySelector("#postImage").value
+
+        const novoPost = {
+            text: textPost,
+            category: postCategory,
+            image: postImage,
+            date: new Date().toLocaleString()
+        }
+
+        posts.unshift(novoPost);
+        mostrarPost()
+
+    }
+//read
+    function mostrarPost() {
+       const listPosts = document.querySelector("#postList");
+       listPosts.innerHTML = "";
+       posts.forEach(pegaItem => {
+        const cardPost = document.createElement("div")
+        cardPost.classList.add("card")
+        cardPost.innerHTML = `
+            <h2>${pegaItem.text}</h2>
+            <img src="${pegaItem.image}"/>
+            <p>Categoria: ${pegaItem.category}</p>
+            <p>Data e hora: ${pegaItem.date}</p>
+            <button>Editar</button> 
+            <button>Deletar</button>
+         `
+         listPosts.append(cardPost)
+       })
+    }
+//update
+    function editarPost() {
+        
+    }
+//delete
+    function deletarPost() {
+        
+    }
